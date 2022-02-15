3072574f5987e8e4ca1bee8ce8437cc861889a3d
Ross Keenan <ross.keenan@protonmail.com>
2022-02-15 09:56:54 -0600

    This now uses the text from the form as a query param against the api.

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -15,20 +15,22 @@ export const GithubProvider = ({ children }) => {
   //dispatch dispatches the action to GithubReducer
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  // Get initial users (testing purposes)
-  const fetchUsers = async () => {
+  // Get search results
+  const searchUsers = async (text) => {
     setLoading();
 
-    const response = await fetch(`${GITHUB_URL}/users`, {
+    const params = new URLSearchParams({ q: text });
+
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
       },
     });
-    const data = await response.json();
+    const { items } = await response.json();
 
     dispatch({
       type: "GET_USERS",
-      payload: data,
+      payload: items,
     });
   };
 
@@ -37,7 +39,7 @@ export const GithubProvider = ({ children }) => {
 
   return (
     <GithubContext.Provider
-      value={{ users: state.users, loading: state.loading, fetchUsers }}
+      value={{ users: state.users, loading: state.loading, searchUsers }}
     >
       {children}
     </GithubContext.Provider>
