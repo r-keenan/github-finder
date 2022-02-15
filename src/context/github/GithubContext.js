58c6c2a20837ccb5c8435c42539dcbfbd29c820b
Ross Keenan <ross.keenan@protonmail.com>
2022-02-15 16:28:41 -0600

    Moved getUsers to actions and refactored.

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -17,25 +17,6 @@ export const GithubProvider = ({ children }) => {
   //dispatch dispatches the action to GithubReducer
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  // Get all users
-  const searchUsers = async (text) => {
-    setLoading();
-
-    const params = new URLSearchParams({ q: text });
-
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    });
-    const { items } = await response.json();
-
-    dispatch({
-      type: "GET_USERS",
-      payload: items,
-    });
-  };
-
   // Get singular user
   const getUser = async (login) => {
     setLoading();
@@ -92,11 +73,8 @@ export const GithubProvider = ({ children }) => {
   return (
     <GithubContext.Provider
       value={{
-        users: state.users,
-        loading: state.loading,
-        user: state.user,
-        repos: state.repos,
-        searchUsers,
+        ...state,
+        dispatch,
         clearUsers,
         getUser,
         getUserRepos,
