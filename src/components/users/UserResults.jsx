7c93a01dd11ef1d1b9d705beb412aa87a679cb67
Ross Keenan <ross.keenan@protonmail.com>
2022-02-16 14:51:06 -0600

    Removed loading from UserResults. Vercel will fail the build with an warnings in the console.

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -3,7 +3,7 @@ import UserItem from "./UserItem";
 import GithubContext from "../../context/github/GithubContext";
 
 function UserResults() {
-  const { users, loading } = useContext(GithubContext);
+  const { users } = useContext(GithubContext);
 
   return (
     <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
